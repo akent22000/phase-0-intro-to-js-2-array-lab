2fc0a6b0df4fd84fa17fb37f91ff61df87b2ffaa
amandalocher <akent22000@gmail.com>
2024-01-11 11:10:03 -0500

    arrays test

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+const cats = [];
+cats.push("Milo", "Otis", "Garfield");
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+ }
+
+ function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+ }
+
+ function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+ }
+
+ function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+ }
+
+ function appendCat(name){
+    const nameAdd=[...cats,name];
+    return nameAdd;
+  }
+
+  function prependCat(name){
+    const nameAddBegin=[name, ...cats];
+    return nameAddBegin
+  }
+
+  function removeLastCat(){
+    const removeLastCat= cats.slice(0,cats.length-1);
+    return removeLastCat;
+  }
+  
+  function removeFirstCat(){
+    const removeFirstCat= cats.slice(1);
+    return removeFirstCat;
+  }
